751229e5424fcb922209daf2bfffa0a23e401acf
Thecharlescz <karel.valenta2003@gmail.com>
2023-04-24 10:46:20 +0200

    xd

--- src/main/java/cz/uhk/models/CsvFileOperations.java
+++ src/main/java/cz/uhk/models/CsvFileOperations.java
@@ -1,5 +1,7 @@
 package cz.uhk.models;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,11 +13,41 @@ public class CsvFileOperations implements FileOperations{
 
     @Override
     public ShoppingList load() {
-        //TODO load from file - 25.4.
+        String line = "";
+        String splitBy = ";";
+        ShoppingList shoppingList = new ShoppingList();
+        //List<ShoppingItem> shoppingList = new ArrayList<>();
+        try
+        {
 
+            BufferedReader br = new BufferedReader(new FileReader(FILE_NAME));
+            line = br.readLine();
+            String[] tokens = line.split(": ");
+            shoppingList.setName(tokens[1]);
+            while ((line = br.readLine()) != null)
+            {
+                tokens = line.split(splitBy);
+                ShoppingItem shoppingItem = new ShoppingItem(tokens[0], Double.parseDouble(tokens[1]), Integer.parseInt(tokens[2]));
+                shoppingItem.setBought(Boolean.parseBoolean(tokens[3]));
+                shoppingList.addItem(shoppingItem);
+            }
+            return shoppingList;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+/*
+        FileReader reader = new FileReader(FILE_NAME);
+        CSVReader csvReader = new CSVReader(reader).skipLines(1);
         List<ShoppingItem> polozkyTemp = new ArrayList<>();
-        polozkyTemp.add(new ShoppingItem("dasda",10,1));
+        String[] tokens = new String[];
 
+        while ((tokens = csvReader.readNext()) != null) {
+            polozkyTemp.add(new ShoppingItem(tokens[1], Double.parseDouble(tokens[2]), Integer.parseInt(tokens[3])));
+            item.setBought(Boolean.parseBoolean(tokens[4]));
+        }
+*/
         return new ShoppingList();
     }
 
